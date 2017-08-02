8c5fd2221902db1ffe88ef536b353502ebbdee06
marekxplore <marek.s.xplore@gmail.com>
2017-08-02 20:28:35 +0200

    eerste commit

---
+++ HelloWorldTestMarek/src/HelloWorldTestMarek.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWorldTestMarek.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Luntemans" << endl; // prints who you really are
+	return 0;
+}
